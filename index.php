6bc22c673bfdc4662685a1aff6dfc1db5d166949
Gabriela Diniz <gadisa10@outlook.com>
2023-01-11 16:51:49 -0300

    update

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ include("script.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/logotipo.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdn.es.gov.br/fonts/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+    <script src="https://kit.fontawesome.com/c2eaecad4c.js"></script>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <title>Cadastro</title>
 </head>
 <body>
@@ -24,7 +28,8 @@ include("script.php");
         <div id="campos">
             <label>Senha:</label>
             <input type="password" name="senha" id="password" minlength="6" maxlength="12" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" onkeyup="verificaForcaSenha();" required>
-            <span class="lnr lnr-eye"></span>
+            <i class="far fa-images"></i>
+            <span id="olhos" class="lnr lnr-eye" onclick="espiando()"></span>
             <span id="password-status"></span>
         </div>
         <input type="submit" value="Cadastrar" id="cadastrar" name="cadastrar">
--- script.php
+++ script.php
@@ -18,3 +18,19 @@
         }
     }
 </script>
+
+<script>
+    function espiando() {
+    let btn = document.querySelector('.lnr-eye');
+    btn.addEventListener('click', function() {
+
+        let input = document.querySelector('#password');
+        if (input.getAttribute('type') == 'password') {
+            input.setAttribute('type', 'text');
+        } else {
+            input.setAttribute('type', 'password');
+        }
+
+    });
+}
+</script>
